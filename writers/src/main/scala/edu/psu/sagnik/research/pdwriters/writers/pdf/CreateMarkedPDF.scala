69605d116482c470d030ce95f41ed269d8964354
sagnik <sagnik1987@gmail.com>
2016-07-14 14:30:06 -0700

    some restructering in writers project

---
+++ writers/src/main/scala/edu/psu/sagnik/research/pdwriters/writers/pdf/CreateMarkedPDF.scala
@@ -0,0 +1,37 @@
+package edu.psu.sagnik.research.pdwriters.writers.pdf
+
+import java.awt.Color
+import java.io.IOException
+
+import edu.psu.sagnik.research.pdsimplify.model.Rectangle
+import org.apache.pdfbox.pdmodel.{PDDocument, PDPage, PDPageContentStream}
+
+/**
+  * Created by schoudhury on 6/27/16.
+  */
+
+
+object CreateMarkedPDF {
+  @throws[IOException]
+  private def drawRect(content: PDPageContentStream, color: Color, rect: Rectangle, fill: Boolean) {
+   content.addRect(rect.x1, rect.y1, rect.x2-rect.x1, rect.y2-rect.y1)
+    if (fill) {
+      content.setNonStrokingColor(color)
+      content.fill()
+    }
+    else {
+      content.setStrokingColor(color)
+      content.stroke()
+    }
+  }
+
+  def apply(docLoc:String,document:PDDocument,pageNum:Int,page:PDPage,bbs:List[Rectangle],color:Color,tElemType:String):Unit={
+    val content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false)
+    bbs.foreach (bb => drawRect (content, color, bb, fill=false))
+
+    content.close()
+    document.save(docLoc.substring(0, docLoc.length - 4) + "-page-"+pageNum+"-"+tElemType+".pdf")
+    println(s"[created]: ${docLoc.substring(0, docLoc.length - 4) + "-page-"+pageNum+"-"+tElemType+".pdf"}")
+  }
+
+}
